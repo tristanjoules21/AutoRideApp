984664934c26469721b6e3c37388b4b21cbdda14
tristanjoules21 <tjpahayahay67260@liceo.edu.ph>
2025-10-26 10:52:40 +0800

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.autorideapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.autorideapp to javafx.fxml;
+    exports com.example.autorideapp;
+}
