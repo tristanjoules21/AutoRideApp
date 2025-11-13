8576f7726c065c5d6e1f47cbf653ab240074e646
Jed Andrei <jasurabasquez66130@liceo.edu.ph>
2025-11-13 22:00:56 +0800

    new

--- src/main/java/com/example/autorideapp/autoride/CarDatabase.java
+++ src/main/java/com/example/autorideapp/autoride/CarDatabase.java
@@ -6,15 +6,9 @@ import java.util.List;
 public class CarDatabase {
     private static final List<Car> cars = new ArrayList<>();
 
-    public static void addCar(Car car) {
-        cars.add(car);
-    }
+    public static void addCar(Car car) { cars.add(car); }
 
-    public static List<Car> getAllCars() {
-        return cars;
-    }
+    public static List<Car> getAllCars() { return cars; }
 
-    public static void removeCar(Car car) {
-        cars.remove(car);
-    }
+    public static void removeCar(Car car) { cars.remove(car); }
 }
